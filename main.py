dbebd2c01d1768a712f5117ca5e3c7665316f3db
Santiago Pardal <santiagopardal@outlook.com>
2022-07-01 09:29:46 -0300

    remove video and improve main

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-import time
 from merger import VideoMerger
 import sys
 import os
-from video import Video
 from videos_iterator import VideosIterator
+from cv2 import VideoCapture, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS
+
+
+class Video:
+    _video: VideoCapture
+
+    def __init__(self, path: str):
+        self._video = VideoCapture(path)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        ret, frame = self._video.read()
+
+        if ret:
+            return frame
+        else:
+            self._video.release()
+            raise StopIteration
+
+    @property
+    def width(self):
+        return int(self._video.get(CAP_PROP_FRAME_WIDTH))
+
+    @property
+    def height(self):
+        return int(self._video.get(CAP_PROP_FRAME_HEIGHT))
+
+    @property
+    def frame_rate(self):
+        return self._video.get(CAP_PROP_FPS)
+
+
+class VideoFactory:
+    def create(self, path: str) -> Video:
+        return Video(path)
 
 
 if __name__ == '__main__':
@@ -16,6 +51,7 @@ if __name__ == '__main__':
 
         print("Merging videos in", folder)
         folder = os.path.abspath(folder)
-        #videos = [Video(os.path.join(folder, path)) for path in sorted(os.listdir(folder)) if path.endswith('.mp4')]
-        merger = VideoMerger(VideosIterator(folder))
+        videos = [Video(os.path.join(folder, path)) for path in sorted(os.listdir(folder)) if path.endswith('.mp4')]
+        factory = VideoFactory()
+        merger = VideoMerger(VideosIterator(videos, factory))
         merger.merge(merged_videos_path)
