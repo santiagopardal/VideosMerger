d05f89e07fd38e4deacaf0efe3bb91ed8ab70fd4
Santiago Pardal <santiagopardal@outlook.com>
2023-01-26 21:41:18 -0300

    add logging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os
 from videos_iterator import VideosIterator
 from cv2 import VideoCapture, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS
+import logging
 
 
 class Video:
@@ -41,17 +42,35 @@ class VideoFactory:
         return Video(path)
 
 
+logging.basicConfig(
+    filename='camerai_merger.log',
+    filemode='a',
+    level=logging.INFO,
+    format="{asctime} {levelname:<8} {message}",
+    style="{"
+)
+
+
 if __name__ == '__main__':
     folders = sys.argv[1:]
+
+    logging.info("Beginning merge")
     for folder in folders:
         merged_videos_path = os.path.join(folder, 'merged_video.mp4')
 
-        if os.path.exists(merged_videos_path):
-            os.remove(merged_videos_path)
+        try:
+            if os.path.exists(merged_videos_path):
+                logging.warning(f"A folder with name {merged_videos_path} already exists, deleting it.")
+                os.remove(merged_videos_path)
 
-        print("Merging videos in", folder)
-        folder = os.path.abspath(folder)
-        videos = [os.path.join(folder, path) for path in sorted(os.listdir(folder)) if path.endswith('.mp4')]
-        factory = VideoFactory()
-        merger = VideoMerger(VideosIterator(videos, factory))
-        merger.merge(merged_videos_path)
+            logging.info("Merging videos in", folder)
+
+            folder = os.path.abspath(folder)
+            videos = [os.path.join(folder, path) for path in sorted(os.listdir(folder)) if path.endswith('.mp4')]
+            factory = VideoFactory()
+            merger = VideoMerger(VideosIterator(videos, factory))
+            merger.merge(merged_videos_path)
+        except Exception as e:
+            logging.error(f"Failed merging videos for folder {folder}: {e}")
+
+    logging.info("Merge finished")
